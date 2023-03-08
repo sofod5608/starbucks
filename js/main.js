f43cbd9b4f8b60c9d41f4026ece55e91d6f0c88f
sofod5608 <qkrcjfgml55@naver.com>
2023-03-08 16:39:22 +0900

    로그인 페이지 완성

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-// 서브메뉴 검색부분 스크립트
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색')
-});
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 // 뱃지부분 스크립트 스크롤하면 없어짐
 const badgeEl = document.querySelector('.header .badges');
@@ -141,5 +126,3 @@ spyEls.forEach(function (spyEl){
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear=document.querySelector('.this-year');
-thisYear.textContent=new Date().getFullYear(); // 2021
